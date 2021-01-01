44311a00be612df3781ab8c6c273d88bfc286272
justinleahy <justinjleahy@gmail.com>
2020-12-31 20:52:46 -0500

    Initial Commit

---
+++ generateModel.py
@@ -0,0 +1,141 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import pathlib
+
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+from tensorflow._api.v2.compat.v1 import ConfigProto
+from tensorflow._api.v2.compat.v1 import InteractiveSession
+
+if __name__ == '__main__':
+
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = InteractiveSession(config=config)
+
+    print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+    data_dir = pathlib.Path('dataset/train')
+    image_count = len(list(data_dir.glob('*/*.jpg')))
+    print(image_count)
+
+    cats = list(data_dir.glob('cat/*'))
+    dogs = list(data_dir.glob('dog/*'))
+
+    batch_size = 10
+    img_height = 250
+    img_width = 250
+
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        label_mode='binary',
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=(img_height,img_width),
+        batch_size=batch_size)
+
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        label_mode='binary',
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
+
+    class_names = train_ds.class_names
+    print(class_names)
+
+#    plt.figure(figsize=(10,10))
+#    for images, labels in train_ds.take(1):
+#        for i in range(9):
+#            ax = plt.subplot(3, 3, i + 1)
+#            plt.imshow(images[i].numpy().astype("uint8"))
+#            plt.title(class_names[labels[i]])
+#            plt.axis("off")
+#
+#    plt.show()
+
+    for image_batch, labels_batch in train_ds:
+        print(image_batch.shape)
+        print(labels_batch.shape)
+        break
+
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+    train_ds = train_ds.cache().shuffle(750).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+
+    normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+    image_batch, labels_batch = next(iter(normalized_ds))
+    first_image = image_batch[0]
+    print(np.min(first_image), np.max(first_image))
+
+    num_classes = 2
+
+    data_augmentation = tf.keras.Sequential(
+        [
+            layers.experimental.preprocessing.RandomFlip("horizontal",
+                                                         input_shape=(img_height,
+                                                                      img_width,
+                                                                      3)),
+            layers.experimental.preprocessing.RandomRotation(0.1),
+            layers.experimental.preprocessing.RandomZoom(0.1)
+        ]
+    )
+
+    model = Sequential([
+        data_augmentation,
+        layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Dropout(0.2),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(num_classes)])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    epochs = 15
+    history = model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=epochs
+    )
+
+#    model.save('models/sequential-model')
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(epochs)
+
+    plt.figure(figsize=(8,8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
---
+++ testModel.py
@@ -0,0 +1,54 @@
+import numpy as np
+import tensorflow as tf
+import pathlib
+import os
+import csv
+
+from tensorflow.keras import layers
+from tensorflow.keras.preprocessing import image
+
+from tensorflow._api.v2.compat.v1 import ConfigProto
+from tensorflow._api.v2.compat.v1 import InteractiveSession
+
+if __name__ == '__main__':
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = InteractiveSession(config=config)
+
+    img_height = 250
+    img_width = 250
+    images = 12500
+
+    model = tf.keras.models.load_model('models/sequential-model')
+    model.summary()
+
+    fields = ['id', 'label']
+    file_name = 'submissions.csv'
+
+    with open(file_name, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(fields)
+
+    for i in range(images):
+        data_dir = pathlib.Path('dataset/test/animal-' + str(i + 1) + '.jpg')
+        test_ds = tf.keras.preprocessing.image.load_img(data_dir, target_size=(img_height, img_width))
+        img_array = tf.keras.preprocessing.image.img_to_array(test_ds)
+        img_array = tf.expand_dims(img_array, 0)
+        predictions = model.predict(img_array)
+        score = tf.nn.softmax(predictions[0])
+
+        class_names = ['cat', 'dog']
+
+        label = np.argmax(score)
+
+        csv_data = [i+1, label]
+
+        with open(file_name, 'a') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(csv_data)
+
+        print(
+            "{} : This image most likely belongs to {} with a {:.2f} percent confidence.".format(i + 1, class_names[label], 100 * np.max(score))
+        )
+
+    exit(0)

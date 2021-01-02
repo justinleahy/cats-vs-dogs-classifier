157a6cf4d8f1dcd856597dc1466e54d01232a8fa
justinleahy <justinjleahy@gmail.com>
2021-01-01 21:38:50 -0500

    Working on a new model using VGG16 application

--- generateModel.py
+++ generateModel.py
@@ -24,9 +24,9 @@ if __name__ == '__main__':
     cats = list(data_dir.glob('cat/*'))
     dogs = list(data_dir.glob('dog/*'))
 
-    batch_size = 10
-    img_height = 250
-    img_width = 250
+    batch_size = 16
+    img_height = 224
+    img_width = 224
 
     train_ds = tf.keras.preprocessing.image_dataset_from_directory(
         data_dir,
@@ -34,7 +34,7 @@ if __name__ == '__main__':
         validation_split=0.2,
         subset="training",
         seed=123,
-        image_size=(img_height,img_width),
+        image_size=(img_height, img_width),
         batch_size=batch_size)
 
     val_ds = tf.keras.preprocessing.image_dataset_from_directory(
@@ -49,27 +49,20 @@ if __name__ == '__main__':
     class_names = train_ds.class_names
     print(class_names)
 
-#    plt.figure(figsize=(10,10))
-#    for images, labels in train_ds.take(1):
-#        for i in range(9):
-#            ax = plt.subplot(3, 3, i + 1)
-#            plt.imshow(images[i].numpy().astype("uint8"))
-#            plt.title(class_names[labels[i]])
-#            plt.axis("off")
-#
-#    plt.show()
+    #    plt.figure(figsize=(10,10))
+    #    for images, labels in train_ds.take(1):
+    #        for i in range(9):
+    #            ax = plt.subplot(3, 3, i + 1)
+    #            plt.imshow(images[i].numpy().astype("uint8"))
+    #            plt.title(class_names[labels[i]])
+    #            plt.axis("off")
+    #
+    #    plt.show()
 
     for image_batch, labels_batch in train_ds:
         print(image_batch.shape)
         print(labels_batch.shape)
         break
-
-    AUTOTUNE = tf.data.experimental.AUTOTUNE
-
-    train_ds = train_ds.cache().shuffle(750).prefetch(buffer_size=AUTOTUNE)
-    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-
-    num_classes = 2
 
     data_augmentation = tf.keras.Sequential(
         [
@@ -77,24 +70,32 @@ if __name__ == '__main__':
                                                          input_shape=(img_height,
                                                                       img_width,
                                                                       3)),
-            layers.experimental.preprocessing.RandomRotation(0.1),
-            layers.experimental.preprocessing.RandomZoom(0.1)
+            layers.experimental.preprocessing.RandomRotation(0.2),
+            layers.experimental.preprocessing.RandomZoom(0.2)
         ]
     )
 
-    model = Sequential([
-        data_augmentation,
-        layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-        layers.Conv2D(16, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(32, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(64, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Dropout(0.2),
-        layers.Flatten(),
-        layers.Dense(128, activation='relu'),
-        layers.Dense(num_classes)])
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+    train_ds = train_ds.cache().shuffle(750).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    augmented_train_ds = train_ds.map(lambda x, y: (data_augmentation(x), y))
+    augmented_val_ds = val_ds.map(lambda x, y: (data_augmentation(x), y))
+
+    num_classes = 2
+
+    # https://arxiv.org/abs/1409.1556
+    model = tf.keras.applications.VGG16(include_top=False, input_shape=(img_height, img_width, 3))
+    for layer in model.layers:
+        layer.trainable = False
+
+    dropout_layer = layers.Dropout(0.2)(model.layers[-1].output)
+    flat_layer = layers.Flatten()(dropout_layer)
+    class_layer = layers.Dense(128, activation='relu', kernel_initializer='he_uniform')(flat_layer)
+    output = layers.Dense(num_classes, activation='sigmoid')(class_layer)
+
+    model = tf.keras.models.Model(inputs=model.inputs, outputs=output)
 
     model.compile(optimizer='adam',
                   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
@@ -104,12 +105,12 @@ if __name__ == '__main__':
 
     epochs = 15
     history = model.fit(
-        train_ds,
-        validation_data=val_ds,
+        augmented_train_ds,
+        validation_data=augmented_val_ds,
         epochs=epochs
     )
 
-#    model.save('models/sequential-model')
+    model.save('models/sequential-model')
 
     acc = history.history['accuracy']
     val_acc = history.history['val_accuracy']
@@ -119,7 +120,7 @@ if __name__ == '__main__':
 
     epochs_range = range(epochs)
 
-    plt.figure(figsize=(8,8))
+    plt.figure(figsize=(8, 8))
     plt.subplot(1, 2, 1)
     plt.plot(epochs_range, acc, label='Training Accuracy')
     plt.plot(epochs_range, val_acc, label='Validation Accuracy')
